c0d1411ed4912b4437c98804d5c51b1dbfd1ba34
atsushi <atsushi@ATSUSHI_WIN>
2021-08-07 12:33:16 +0900

    プロジェクト ファイルを追加します。

---
+++ BookApiBack/Models/BookApiContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookApiBack.Models
+{
+    public class BookApiContext : DbContext
+    {
+        public BookApiContext(DbContextOptions<BookApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BookItem> BookItems { get; set; }
+    }
+}
---
+++ BookApiBack/Models/BookItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BookApiBack.Models
+{
+    public class BookItem
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Publisher { get; set; }
+        public DateTime PublishedDate { get; set; }
+        public string Image { get; set; }
+    }
+}
